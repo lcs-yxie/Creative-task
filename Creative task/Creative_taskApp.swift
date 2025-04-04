cdfa33e240920cd262532d0c9340503a9144da5e
Yukun Xie <yxie@lcs.on.ca>
2025-04-04 14:39:22 -0400

    Initial Commit

---
+++ Creative task/Creative_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Creative_taskApp.swift
+//  Creative task
+//
+//  Created by Yukun Xie on 2025/4/4.
+//
+
+import SwiftUI
+
+@main
+struct Creative_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
